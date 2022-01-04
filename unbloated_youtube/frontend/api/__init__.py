05436166e7bcace04dbe250ed4a18e87199660db
secrecyflag <96260868+secrecyflag@users.noreply.github.com>
2022-01-04 13:49:31 +0000

    Implemented headers

--- unbloated_youtube/frontend/api/__init__.py
+++ unbloated_youtube/frontend/api/__init__.py
@@ -1,7 +1,8 @@
 from unbloated_youtube.backend import ytcfg
 from unbloated_youtube.backend.constants import Urls
+from unbloated_youtube.backend import headers
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/532.0 (KHTML'}  # TODO: implement headers.py
+HEADERS = {"User-Agent": headers.generate()}
 YTCFG_OBJ = ytcfg.YtConfig(Urls.YOUTUBE_URL, headers=HEADERS, start=True, more=True, is_video=False)
 YTCFG_OBJ.getconfig()
 
