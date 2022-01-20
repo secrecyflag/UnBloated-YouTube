123c71a9bb980ff281fab6d82257aca00d95502d
secrecyflag <96260868+secrecyflag@users.noreply.github.com>
2022-01-20 22:49:28 +0000

    Created ytdata object

--- unbloated_youtube/frontend/api/__init__.py
+++ unbloated_youtube/frontend/api/__init__.py
@@ -1,8 +1,11 @@
-from unbloated_youtube.backend import ytcfg
+from unbloated_youtube.backend import ytcfg, ytdata
 from unbloated_youtube.backend.constants import Urls
 from unbloated_youtube.backend import headers
 
 HEADERS = {"User-Agent": headers.generate()}
 YTCFG_OBJ = ytcfg.YtConfig(Urls.YOUTUBE_URL, headers=HEADERS, start=True, more=True, is_video=False)
 YTCFG_OBJ.getconfig()
+
+YTDATA_OBJ = ytdata.YtData(Urls.YOUTUBE_URL, headers=HEADERS, start=True, is_video=False)
+YTDATA_OBJ.getdata()
 
