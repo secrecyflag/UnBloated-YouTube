37034588b5f98601f6abffa07b04355787594343
secrecyflag <96260868+secrecyflag@users.noreply.github.com>
2022-01-20 22:51:43 +0000

    Added support for playback speed

--- unbloated_youtube/frontend/static/js/player.js
+++ unbloated_youtube/frontend/static/js/player.js
@@ -10,16 +10,18 @@ audio.volume = 0.2;
 volume.addEventListener("mousemove", (e)=> {  // volume slider
     audio.volume = e.target.value;            
 })
-video.addEventListener("pause", (e)=> {  // if the video is paused
+video.addEventListener("pause" || "waiting", (e)=> {  // if the video is paused
     audio.pause();
+    audio.currentTime = video.currentTime;
 })
 video.addEventListener("play", (e)=> {  // if the video is on, then:
+    audio.currentTime = video.currentTime;
     audio.play();
 })
 video.addEventListener("seeked", (e)=> {  // if the user changed the position in video, update also in the audio element
     audio.currentTime = video.currentTime;
 })
-video.addEventListener("waiting", (e)=> {  // if the video needs to buffer
-    audio.currentTime = video.currentTime;
+video.addEventListener("ratechange", (e)=> {  // if the video playback speed changed
+    audio.playbackRate = video.playbackRate;
 })
 
